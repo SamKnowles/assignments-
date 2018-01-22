9c10e8006ec1122d3f6ea055dd8a3c04f30ca8d9
Sam Knowles <samuelkcknowles@gmail.com>
2018-01-22 07:50:57 -0700

    Finished the react roto router app

--- exercises/react-roto-router/src/App/index.js
+++ exercises/react-roto-router/src/App/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Switch, Route, Link } from "react-router-dom";
-import
+import { Switch, Route } from "react-router-dom";
+import Home from '../pages/Home';
+import Services from '../pages/Services';
+import About from '../pages/About';
+import Navbar from './Navbar';
 
 
 function App(props) {
     return (
         <div>
-            
+            <Navbar />
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/about' component={About} />
+                <Route path='/services' component={Services} />
+            </Switch>
         </div>
     )
 }
