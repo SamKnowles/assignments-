f7d985752ded8d9a17e0fb6ff1f3b8b4042616b9
Sam Knowles <samuelkcknowles@gmail.com>
2019-02-06 13:58:42 -0700

    updated seasercipher exercise

--- exercises/ceaserCipher/main.js
+++ exercises/ceaserCipher/main.js
@@ -19,3 +19,27 @@ function myReduce(arr, fn, initial) {
     return result;
 }  
 
+// const ask = require('readline-sync');
+
+// let text = ask.question('Enter the text you would like to encode');
+// let shift = ask.question('Enter a number');
+
+// let output = '';
+// let regex = /[A-Z]/;
+// text = text.toUpperCase();
+// shift = Math.round(shift % 26);
+// console.log('shift:', shift);
+
+// for(let i = 0; i < text.length; i++) {
+//     let newCharCode = text.charCodeAt(i) + shift;
+//     if(regex.test(text[i])) {
+//         if(newCharCode > 90) {
+//             let diff = newCharCode - 90;
+//             newCharCode = 64 + diff;
+//         }
+//         output += String.fromCharCode(newCharCode);
+//     } else {
+//         output += text[i];
+//     }
+// }
+// console.log('Here is your encoded message: ', output);
--- exercises/escapeRoom/index.js
+++ exercises/escapeRoom/index.js
@@ -1,74 +1,74 @@
-const rs = require("readline-sync");
-
-console.log("Youre in a room. There is a girl, some hidden cash, and the slim possibility of becoming un-single.");
-
-let inv = [];
-let temporarilySingle = true;
-
-while (temporarilySingle) {
-    let userinput = rs.question("My name is Mercedes.  What's up? ");
-    userinput = userinput.toLowerCase();
-
-    if (userinput.includes("yo") && userinput.includes("gurl")) {
-        console.log("... Hard pass.");
-        temporarilySingle = false;
-        console.log("Theres no hope for you, pleb. Youre permanently single.");
-    }
-    else if (userinput.includes("im rich") && userinput.includes("youre beautiful")) {
-        if (inv.includes("cash money")) {
-            console.log("Okaaay boo. I like where this is going!");
-            break;
-        }
-        else {
-            console.log("Gross. No... Just... no.");
-        }
-    }
-    else if (userinput.includes("take") && userinput.includes("cash money")) {
-        console.log("Great find playa!");
-        inv.push("cash money");
-        console.log(inv);
-    }
-    else if (userinput.includes("look")) {
-        console.log("Youre in a room. There is a girl, some hidden cash, and the slim possibility of becoming un-single.");
-    }
-    else {
-        console.log("Wrong words fool!");
-    }
-}
-
 // const rs = require("readline-sync");
 
-// console.log("You are in a locked room.\nThere is a door, a hidden key, and a hole in the wall.");
+// console.log("Youre in a room. There is a girl, some hidden cash, and the slim possibility of becoming un-single.");
 
 // let inv = [];
-// let isAlive = true;
+// let temporarilySingle = true;
 
-// while(isAlive) {
-//     let userinput = rs.question("What would you like to do? ");
+// while (temporarilySingle) {
+//     let userinput = rs.question("My name is Mercedes.  What's up? ");
 //     userinput = userinput.toLowerCase();
 
-//     if (userinput.includes("hole") && userinput.includes("put")) {
-//         console.log("Sorry Sucka! You dead!  Holes Kill!");
-//         isAlive = false;
-//         console.log("Also, There Is No god");
-//     } 
-//     else if(userinput.includes("open") && userinput.includes("door")) {
-//         if(inv.includes("key")) {
-//             console.log("Congrats you WIN the game!! You get three wishes!");
+//     if (userinput.includes("yo") && userinput.includes("gurl")) {
+//         console.log("... Hard pass.");
+//         temporarilySingle = false;
+//         console.log("Theres no hope for you, pleb. Youre permanently single.");
+//     }
+//     else if (userinput.includes("im rich") && userinput.includes("youre beautiful")) {
+//         if (inv.includes("cash money")) {
+//             console.log("Okaaay boo. I like where this is going!");
 //             break;
-//         } 
-//         else {
-//             console.log("You need a key to do that.  It's one of the only things in the room.  You can just take it.");
 //         }
-//     } 
-//     else if(userinput.includes("take") && userinput.includes("key")) {
-//         console.log("You found the key but is it the key?");
-//         inv.push("key");
-//     } 
-//     else if(userinput.includes("look")) {
-//         console.log("You are in a locked room.\nThere is a door, a 'hidden' key, and a hole in the wall.");        
-//     } 
+//         else {
+//             console.log("Gross. No... Just... no.");
+//         }
+//     }
+//     else if (userinput.includes("take") && userinput.includes("cash money")) {
+//         console.log("Great find playa!");
+//         inv.push("cash money");
+//         console.log(inv);
+//     }
+//     else if (userinput.includes("look")) {
+//         console.log("Youre in a room. There is a girl, some hidden cash, and the slim possibility of becoming un-single.");
+//     }
 //     else {
-//         console.log("Sorry but that isn't one of the commands");
+//         console.log("Wrong words fool!");
 //     }
 // }
+
+const rs = require("readline-sync");
+
+console.log("You are in a locked room.\nThere is a door, a hidden key, and a hole in the wall.");
+
+let inv = [];
+let isAlive = true;
+
+while(isAlive) {
+    let userinput = rs.question("What would you like to do? ");
+    userinput = userinput.toLowerCase();
+
+    if (userinput.includes("hole") && userinput.includes("put")) {
+        console.log("Sorry Sucka! You dead!  Holes Kill!");
+        isAlive = false;
+        console.log("Also, There Is No god");
+    } 
+    else if(userinput.includes("open") && userinput.includes("door")) {
+        if(inv.includes("key")) {
+            console.log("Congrats you WIN the game!! You get three wishes!");
+            break;
+        } 
+        else {
+            console.log("You need a key to do that.  It's one of the only things in the room.  You can just take it.");
+        }
+    } 
+    else if(userinput.includes("take") && userinput.includes("key")) {
+        console.log("You found the key but is it the key?");
+        inv.push("key");
+    } 
+    else if(userinput.includes("look")) {
+        console.log("You are in a locked room.\nThere is a door, a 'hidden' key, and a hole in the wall.");        
+    } 
+    else {
+        console.log("Sorry but that isn't one of the commands");
+    }
+}
