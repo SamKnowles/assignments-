8296297449a222cc68e22a88754cef847faee94a
Sam Knowles <samuelkcknowles@gmail.com>
2019-02-10 18:29:02 -0700

    updated anticaps app

--- warmup/warmUpAntiCaps/README.md
+++ warmup/warmUpAntiCaps/README.md
@@ -1,1 +1,8 @@
-A function that switches lowercase to Uppercase and vise versa 
+Write a function called antiCaps, which accepts a string and manipulates it as follows.
+
+Uppercase characters will become lowercase.
+Lowercase characters will become uppercase.
+
+antiCaps('Hello')  // hELLO
+antiCaps('racEcar')  // RACeCAR
+antiCaps('bAnAnA')  // BaNaNa
--- warmup/warmUpAntiCaps/main.js
+++ warmup/warmUpAntiCaps/main.js
@@ -1,3 +1,8 @@
+//Write a function called antiCaps, which accepts a string and manipulates it as follows.
+
+//Uppercase characters will become lowercase.
+//Lowercase characters will become uppercase.
+
 function isCaps(str) {
     str = str.split('');
     for (var i = 0; i < str.length; i++) {
@@ -11,3 +16,36 @@ function isCaps(str) {
     return str.join('');
   }
   console.log(isCaps("fdddHGHGHghggHGhghGGGGG"));
+
+  //Have the function return double letters if originally uppercase
+
+function isCaps(str) {
+    str = str.split('');
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === str[i].toUpperCase()) {
+            str[i] = str[i].toLowerCase() + str[i].toLowerCase();
+        }
+        else if (str[i] === str[i].toLowerCase()) {
+            str[i] = str[i].toUpperCase();
+        }
+    }
+    return str.join('');
+  }
+  console.log(isCaps("abcABC"));
+
+
+  //Have the function return the letter and a symbol if originally lowercase.
+  
+  function isCaps(str) {
+    str = str.split('');
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === str[i].toUpperCase()) {
+            str[i] = str[i].toLowerCase();
+        }
+        else if (str[i] === str[i].toLowerCase()) {
+            str[i] = str[i].toUpperCase() + '$';
+        }
+    }
+    return str.join('');
+  }
+  console.log(isCaps("abdABC"));
